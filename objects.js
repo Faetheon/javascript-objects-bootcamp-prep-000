f70e944fa342bcc719a67f0d610d14dd43788f66
Nathan Hill <nateohill@gmail.com>
2018-05-18 22:05:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,4 @@
-var playlist = { artist: "Moi", song: "The River" };
 function updatePlaylist(playlist, artistName, songTitle) {
-  this.playlist.artist(artistName);
-  this.playlist.song(songTitle);
+  playlist = { artist: "Moi", song: "The River" };
+  
 }
